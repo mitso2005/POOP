094350f42d9d8a5b376344c8358e62978d621720
mitso2005 <gpetrakis343@gmail.com>
2026-05-01 23:04:18 +1000

    feat: SafetyAsset overall count and serial array

--- assets/base.py
+++ assets/base.py
@@ -1,10 +1,17 @@
-class Asset:
+class SafetyAsset:
+    total_assets = 0
+    all_serials = []
+    
     def __init__(self, serial_number: int, location: str, last_inspected: str = 'Never'):
         self.serial_number = serial_number
         self.location = location
         self.last_inspected = last_inspected
+        SafetyAsset.total_assets += 1
+        SafetyAsset.all_serials.append(serial_number)
         
     def describe(self):
         print(f"[{self.serial_number}] {self.location} (last inspected: {self.last_inspected})\n")
+        
+    
         
     
--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-from assets.base import Asset
+from assets.base import SafetyAsset
 
-test_1 = Asset(1235, 'Melbourne', '23/04/23')
+test_1 = SafetyAsset(1235, 'Melbourne', '23/04/23')
+test_2 = SafetyAsset(1235, 'Melbourne', '23/04/23')
 
 test_1.describe()
+
+print(SafetyAsset.total_assets)
+print(SafetyAsset.all_serials)
